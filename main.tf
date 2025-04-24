08c0ef143b5357d0d67acd336b6a191e00611cb8
NARAYAN PRASAD  PRADHAN <narayanprasadaws@yahoo.com>
2025-04-24 10:39:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,15 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
 count = 5
-ami = "ami-07a6f770277670015"
-instance_type = "t2.micro"
+ami = "ami-0ddc798b3f1a5117e"
+instance_type = "t2.medium"
+key_name = "newkey"
+vpc_security_group_ids = ["sg-05f044979e305302e"]
+tags = {
+Name = var.instance_names[count.index]
+}
+}
+
+variable "instance_names" {
+default = ["jenkins", "nexus", "tomcat-1", "tomcat-2", "Monitoring server"]
 }
